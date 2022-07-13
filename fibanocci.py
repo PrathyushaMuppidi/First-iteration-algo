a50bbeab741b7f35ae92c251ba033feb2fa084f2
Prathyusha Muppidi <mprathyusha@gmail.com>
2022-07-13 15:12:01 -0400

    fibanocci.py

---
+++ fibanocci.py
@@ -0,0 +1,9 @@
+def get_fib(position):
+    
+    if position == 0:
+        return 0
+    elif position == 1:
+        return 1
+    else:
+        return get_fib(position - 1) + get_fib(position-2)
+       
